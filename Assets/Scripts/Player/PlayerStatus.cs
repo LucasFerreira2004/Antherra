ee34b2d61435b3b8b57ebac4685b7700a58c940c
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-07-13 15:58:44 -0300

    add: barra de vida em coracoes

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,17 +1,34 @@
+using System;
 using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+    public static event Action OnPlayerDamaged;
+    public static event Action OnPlayerDeath;
+    public static event Action OnPlayerHealed;
+    public static event Action OnPlayerHealthIncresed;
+
     [SerializeField] private float speed = 8;
-    [SerializeField] private int maxHealth;
-    [SerializeField] private int currentHealth;
-    [SerializeField] private float bulletDamage;
+    [SerializeField] private int maxHealth = 8;
+    [SerializeField] private int currentHealth = 8;
+    [SerializeField] private float bulletDamage = 2;
     [SerializeField] private float bulletFireRate = 2;
     [SerializeField] private float bulletSpeed = 10;
     [SerializeField] private float bulletRange = 1;
+    public void Start()
+    {
+        speed = 8;
+        maxHealth = 8;
+        currentHealth = 8;
+        bulletDamage = 2;
+        bulletFireRate = 2;
+        bulletSpeed = 10;
+        bulletRange = 1;
+    }
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        OnPlayerDamaged?.Invoke();
         if (currentHealth <= 0)
         {
             Die();
@@ -20,7 +37,11 @@ public class PlayerStatus : MonoBehaviour
 
     public void Heal(int amount)
     {
-        currentHealth += amount;
+        int totalHealthAfterHeal = currentHealth + amount;
+        if (totalHealthAfterHeal > maxHealth)
+            currentHealth = maxHealth;
+        else
+            currentHealth += amount;
     }
 
     private void Die()
