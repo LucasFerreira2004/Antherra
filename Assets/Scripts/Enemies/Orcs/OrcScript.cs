f13d216129573866bcb776c82d6d6213e9a495a1
daniel christopher <daniel.2005.christopher@gmail.com>
2025-07-16 12:51:09 -0300

    feat: create hurt and death animation triggers for orcs and set theirs life status

--- Assets/Scripts/Enemies/Orcs/OrcScript.cs
+++ Assets/Scripts/Enemies/Orcs/OrcScript.cs
@@ -7,7 +7,7 @@ public enum OrcState
     Moving,
 }
 
-public class OrcScript : MonoBehaviour
+public class OrcScript : MonoBehaviour, ITakeDamage
 {
     [Header("Actions")]
     [SerializeField] private ScriptableObject movementSO;
@@ -21,20 +21,28 @@ public class OrcScript : MonoBehaviour
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private Transform player;
 
+
     public Rigidbody2D Rigidbody => rigidBody;
     public Animator Animator => animator;
     public Transform Player => player;
+
+    [Header("Attributes")]
+    [SerializeField] private float maxHealth = 10f;
+    [SerializeField] private float currentHealth;
 
     public Vector2 MoveDirection;
     public Vector2 LastDirection = Vector2.down;
 
     public OrcState State { get; private set; } = OrcState.Waiting;
 
+    private bool CanMove = true;
+
     private void Start()
     {
         movement = movementSO as IOrcMovement;
         attack = attackSO as IOrcAttack;
         player = GameObject.FindWithTag("Player").transform;
+        currentHealth = maxHealth;
     }
 
     private void Update()
@@ -62,7 +70,7 @@ public class OrcScript : MonoBehaviour
     {
         float distance = Vector2.Distance(transform.position, player.position);
 
-        if (distance <= movement.GetDistanceRadius())
+        if (distance <= movement.GetDistanceRadius() && CanMove)
         {
             SetState(OrcState.Moving);
             return;
@@ -92,5 +100,24 @@ public class OrcScript : MonoBehaviour
     {
         MoveDirection = Vector2.zero;
         animator.SetFloat("Speed", MoveDirection.magnitude);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        SetState(OrcState.Waiting);
+        currentHealth -= damage;
+        if (currentHealth <= 0f)
+        {
+            animator?.SetTrigger("Death");
+            CanMove = false;
+            return;
+        }
+
+        animator?.SetTrigger("Hurt");
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
