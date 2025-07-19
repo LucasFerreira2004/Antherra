e0dded9e1df06466765b73a43970369862ca9768
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-07-19 00:10:56 -0300

    add: funcionalidade de fechar e abrir salas

--- Assets/Scripts/Rooms/RoomTranstion.cs
+++ Assets/Scripts/Rooms/RoomTranstion.cs
@@ -1,24 +1,32 @@
+using System;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography.X509Certificates;
 using Unity.VisualScripting;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class RoomTransition : MonoBehaviour
 {
+    [SerializeField] private GameObject roomExitBlockPrefab;
+    [SerializeField] private Transform roomExitBlockPositon;
+    private GameObject roomExitBlock;
     private RoomManager currentRoom;
     private bool open;
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
         currentRoom = GetComponentInParent<RoomManager>();
         currentRoom.ActivateRoom();
+        roomExitBlock = Instantiate(roomExitBlockPrefab, roomExitBlockPositon.position, Quaternion.identity);
+        GetComponent<Collider2D>().enabled = false;
     }
 
     public void SetOpen(bool open)
     {
-        if (!open)
+        if (open)
         {
-            return;
+            Destroy(roomExitBlock);
+            Destroy(this.gameObject);
         }
     }
 }
