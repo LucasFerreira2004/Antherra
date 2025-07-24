161fab96c6c5c8bde9d538aa4ec342dc1737eb0b
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-07-24 19:59:05 -0300

    add: belts powerUpts

--- Assets/Scripts/PowerUps/PowerUpsListData.cs
+++ Assets/Scripts/PowerUps/PowerUpsListData.cs
@@ -1,12 +1,22 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "PowerUps/PowerUps List")]
 public class PowerUpsListData : ScriptableObject
 {
     [SerializeField] private List<GameObject> itemsPrefabs;
+    [SerializeField] private List<PowerUpsListData> otherLists;
 
-
+    public void Awake()
+    {
+        foreach (PowerUpsListData list in otherLists) {
+            foreach (var itemPerfab in list.ItemsPrefabs)
+            {
+                itemsPrefabs.Add(itemPerfab);
+            }
+        }
+    }
     public List<GameObject> ItemsPrefabs
     {
         get => itemsPrefabs;
